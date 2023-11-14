be6405c644a300fd89d5e15492bd66a9a29eab78
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-14 10:16:32 -0800

    removed broken code

--- script.js
+++ script.js
@@ -10,83 +10,24 @@ const allClear = document.getElementById('ac');
 
 let firstNum = '';
 let secondNum = '';
+let currentOperand = 'first';
+let currentOperator = '';
 let total = 0;
-let currentOperand = '';
-let continuedOperation = false;
 
-const triggerOperationUpdate = () => {
-	if (firstNum.length === 0) {
-		operation.textContent = '';
-	}
-	operation.textContent = `${firstNum} ${currentOperand} ${secondNum}`;
-};
+const triggerOperationUpdate = () => {};
 
 const addOperatorEvents = operators.forEach((operator) => {
-	operator.addEventListener('click', () => {
-		if (firstNum.length !== 0) {
-			currentOperand = operator.textContent;
-			continuedOperation = true;
-			triggerOperationUpdate();
-		}
-	});
+	operator.addEventListener('click', () => {});
 });
 
 const addNumberEvent = numbers.forEach((number) => {
-	number.addEventListener('click', () => {
-		// if (total !== 0) {
-		// 	firstNum = '';
-		// 	firstNum += number.textContent;
-		// 	triggerOperationUpdate();
-		// }
-		if (!continuedOperation) {
-			firstNum += number.textContent;
-			triggerOperationUpdate();
-		} else {
-			secondNum += number.textContent;
-			triggerOperationUpdate();
-		}
-	});
+	number.addEventListener('click', () => {});
 });
 
-decimal.addEventListener('click', () => {
-	if (secondNum.length === 0 && !firstNum.includes('.')) {
-		firstNum += '.';
-		triggerOperationUpdate();
-	} else if (!secondNum.includes('.')) {
-		console.log('firing');
-		secondNum += '.';
-		triggerOperationUpdate();
-	}
-});
+decimal.addEventListener('click', () => {});
 
-equals.addEventListener('click', () => {
-	if (firstNum.length !== 0 && secondNum.length !== 0) {
-		total = eval(`${firstNum} ${currentOperand} ${secondNum}`);
-		result.textContent = total;
-		currentOperand = '';
-		secondNum = '';
-		firstNum = total.toString();
-		continuedOperation = true;
-	}
-});
+equals.addEventListener('click', () => {});
 
-clear.addEventListener('click', () => {
-	if (secondNum.length === 0) {
-		firstNum = '';
-		result.textContent = '';
-		triggerOperationUpdate();
-	} else {
-		secondNum = '';
-		result.textContent = '';
-		triggerOperationUpdate();
-	}
-});
+clear.addEventListener('click', () => {});
 
-allClear.addEventListener('click', () => {
-	firstNum = '';
-	currentOperand = '';
-	secondNum = '';
-	total = 0;
-	result.textContent = '';
-	triggerOperationUpdate();
-});
+allClear.addEventListener('click', () => {});
